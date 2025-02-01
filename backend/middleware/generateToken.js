00814a7404c7da2b2cf1ec17c256d6518798e3f5
AnoushkaSamanta <anoushka1701@gmail.com>
2025-02-01 11:06:28 +0530

    Some changes for jwt

--- backend/middleware/generateToken.js
+++ backend/middleware/generateToken.js
@@ -1,8 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (email) => {
-  console.log(process.env.JWT_SECRET);
-  
+ 
   return jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: "24h" });
 };
 
